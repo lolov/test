45c68d47a0ecd37d5853dcacaa0981b86a57ded0
lolo <lolo@lolo-HP>
2017-09-05 19:19:43 +0200

    test undo

--- index.php
+++ index.php
@@ -1,19 +1,50 @@
-<?php  echo 'bonjour'; 
-
-echo'ajouter cette ligne';
-echo 'bababababa';
-echo'je suis serveur';
-
-echo'ligne serveur , je suis dans la branch suivi';
-
-
-echo 'encore ajouter une ligne, cette linge en conflit';
-
-
-echo 'asdfsdfsdf_totototot';
-
-
-echo 'je suis sur branch master';
-
-
-?>
+<?php 
+    
+    function lire_dossier(){
+    
+$tab = scandir ('/home/lolo/daec' );
+foreach($tab as $k => $v){
+    if($k != '.' && $k != '..'){
+     
+        $handle = @fopen("/home/lolo/daec/".$v, "r");
+        if ($handle) {
+            $affiche = false;
+            $n = 0;
+            while (($buffer = fgets($handle, 4096)) !== false) {
+                if(!$affiche){
+                    if(strpos($buffer, "#UP") !== false) {
+                        $affiche = true;
+                    }
+                }
+               if($affiche){
+                   $n++;
+               }
+                
+                if($affiche && $n > 1){
+                    
+                     if((strpos($buffer, "#DOWN") === false) &&  (strpos($buffer, "INSERT") === false)
+                         &&   (strpos($buffer, "UPDATE") === false) 
+                             
+                             ){
+                    
+                 
+                      echo $buffer; echo '<br>';
+                     }
+                }
+                   
+                    
+                  
+                    } 
+               
+            }
+            if (!feof($handle)) {
+                echo "Erreur: fgets() a échoué\n";
+            }
+            fclose($handle);
+        }
+    }
+    }
+    
+    echo 'sdfsdfsdf';
+    
+ 
